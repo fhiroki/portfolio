2d4e7a0e8e82abe012575d077f2d90cda6c7cff4
fhiroki <hiroki976@gmail.com>
2020-10-26 15:37:09 +0900

    fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/portfolio/'
+  : '/',
   outputDir: "docs",
 }
