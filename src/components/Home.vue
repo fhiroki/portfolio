e053bcfefd85fbd6a565fa138dbd1c6f64eb755c
fhiroki <hiroki976@gmail.com>
2020-10-26 15:21:55 +0900

    Design adjustment

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,19 +19,37 @@
                 v-text="item.yearMonth"
               ></span>
             </template>
+
             <v-card class="elevation-2">
               <v-card-title
                 :class="`headline font-weight-bold ${item.color}--text`"
               >
                 <div>{{ item.title }}</div>
               </v-card-title>
-              <v-card-text v-if="item.content" class="card-text">
+
+              <v-card-text v-if="item.content || item.works" class="card-text">
                 <div v-html="item.content"></div>
+
                 <v-expansion-panels popout class="expansion-panel">
                   <v-expansion-panel v-for="(work, i) in item.works" :key="i">
-                    <v-expansion-panel-header>{{
-                      work.title
-                    }}</v-expansion-panel-header>
+                    <v-expansion-panel-header>
+                      <v-row no-gutters>
+                        <v-col cols="5" style="padding-top: 5px">
+                          {{ work.title }}
+                        </v-col>
+                        <v-col cols="4">
+                          <v-icon>mdi-code-tags</v-icon>
+                          <span v-for="(language, i) in work.languages" :key="i">
+                            <v-icon small>mdi-language-{{ language }}</v-icon>
+                          </span>
+                        </v-col>
+                        <v-col cols="3">
+                          <v-icon>mdi-calendar</v-icon>
+                          {{ work.period }}
+                        </v-col>
+                      </v-row>
+                    </v-expansion-panel-header>
+
                     <v-expansion-panel-content>
                       <div v-html="work.content"></div>
                     </v-expansion-panel-content>
@@ -55,15 +73,27 @@ export default {
       {
         yearMonth: "2016/4",
         title: "横浜市立大学入学",
-        url: "https://www.yokohama-cu.ac.jp/",
         icon: "mdi-school",
         color: "light-green",
-        content: "",
+        works: [
+          {
+            title: "卒業研究",
+            period: "大学３〜４年",
+            languages: ["python"],
+            content: `卒業研究として、\
+<a href="https://github.com/fhiroki/analyze-leed-pattern" target="_blank" rel="noopener noreferrer">画像認識ソフト</a>\
+を開発しました。このプロジェクトは、実験の効率化を図るもので、\
+LEEDと呼ばれる電子回折装置を用いて撮影された画像から、\
+回折スポットを自動で検出し、解析を行います。\
+Python で CLI ツールとして実装しており、画像の解析には OpenCV を使用しました。\
+このプロジェクトでは機械学習は用いていませんが、\
+クラシカルな画像処理でも精度の高い検出結果を得ることができました。`,
+          },
+        ],
       },
       {
         yearMonth: "2017/4",
         title: "株式会社Tagbangers入社（アルバイト）",
-        url: "https://www.tagbangers.co.jp/",
         icon: "mdi-office-building",
         color: "red",
         content:
@@ -74,7 +104,9 @@ export default {
 技術への興味を持つきっかけをつくってくれました。",
         works: [
           {
-            title: "０〜３ヶ月 社内研修",
+            title: "社内研修",
+            period: "０〜３ヶ月",
+            languages: ["java"],
             content:
               "会社に入ってから約３ヶ月間は、研修期間として、様々な課題にチャレンジしました。\
 タグバンガーズはWeb開発が中心の会社で、主な言語としてはJava、フレームワークはSpringを使用しているので、まずは、Javaで課題を出していただきました。\
@@ -96,7 +128,9 @@ export default {
 そうして少しずつプログラムに対する理解を深めていき、約3ヶ月後からはいよいよ実務に入らせていただくことになりました。",
           },
           {
-            title: "３〜６ヶ月 iOSアプリの保守、開発",
+            title: "iOSアプリの保守、開発",
+            period: "３〜６ヶ月",
+            languages: ["swift"],
             content:
               "約３ヶ月経った頃には、先輩のエンジニアにご指導いただきながら、iOSアプリの保守や開発を行いました。\
 ここでは、現場でどのようなツールを使い、どのように仕事を進めていくのかを学ぶことができました。\
@@ -108,7 +142,9 @@ export default {
 一見当たり前のことのようにも思えますが、実際にやってみるまではデバッグの大変さを理解していなかったので、大きな学びの１つだと思います。",
           },
           {
-            title: "６〜１０ヶ月 スマホアプリの開発",
+            title: "スマホアプリの開発",
+            period: "６〜１０ヶ月",
+            languages: ["javascript"],
             content:
               "NativeScriptという、iOSとAndroidの両方を同時に開発できるフレームワークを用いて、スマホアプリの開発を行いました。\
 まず画面のモックアップを作り、その後でAPIコール部分を作りました。\
@@ -123,7 +159,9 @@ export default {
 これらの１手間がかなり厳しく、色々な手直しが入り、正直心が折れそうになりました。その分、リリースが完了した時の達成感は一入でした。",
           },
           {
-            title: "８ヶ月頃 Spring One参加（サンフランシスコ）",
+            title: "Spring One参加（サンフランシスコ）",
+            period: "８ヶ月頃",
+            languages: ["java"],
             content:
               "インターンを始めて約半年が経った頃、社内研修としてSpring Oneに参加するため、サンフランシスコに連れていってもらいました。\
 海外に行くのは生まれて初めてだったので、非常に貴重な経験をさせてもらったと思っています。\
@@ -138,7 +176,9 @@ export default {
 「おもてなし」の精神は口だけではなく、本当なんだと実感しました。",
           },
           {
-            title: "１０〜１２ヶ月 Webアプリ・APIの開発",
+            title: "Webアプリ・APIの開発",
+            period: "１０〜１２ヶ月",
+            languages: ["java", "javascript"],
             content:
               "WebアプリはAngularというJavaScriptのフレームワーク、APIはSpringで実装しました。\
 AngularはNativeScriptでも使っていたので、比較的習得が早かった気がします。\
@@ -147,7 +187,9 @@ Springを本格的に触ったのはこれが初めてで、それもSpring Data
 Springに関しては奥が深く、まだまだ勉強すべきことがたくさんあるので、今後の学習課題の１つだと思っています。",
           },
           {
-            title: "１２〜１５ヶ月 機械学習を用いた画像認識アプリの開発",
+            title: "機械学習を用いた画像認識アプリの開発",
+            period: "１２〜１５ヶ月",
+            languages: ["python"],
             content:
               "機械学習のライブラリを用いた画像認識の組み込みアプリを開発しました。\
 前々から機械学習には興味があったので、このプロジェクトが決まった時は嬉しかったです。\
@@ -156,7 +198,9 @@ Springに関しては奥が深く、まだまだ勉強すべきことがたく�
 その中でも、Coursera の Machine Learning の講義は大変勉強になりました。",
           },
           {
-            title: "１５〜１８ヶ月 Webアプリ（モックアップ）の開発",
+            title: "Webアプリ（モックアップ）の開発",
+            period: "１５〜１８ヶ月",
+            languages: ["javascript", "css3"],
             content:
               "最後のプロジェクトに臨む頃には、Gitの使い方やタスク管理の仕方などがある程度習熟してきて、基本的な操作は問題なく行えるようになりました。\
 正直にいうと、最初の頃はあまり理解せずに、GUI頼みでそれらのツールを使っており、だいぶ経ってからきちんと理解するに至りました。\
@@ -176,15 +220,14 @@ Springに関しては奥が深く、まだまだ勉強すべきことがたく�
       {
         yearMonth: "2019/8",
         title: "株式会社Taktpixel入社（アルバイト）",
-        url: "https://taktpixel.co.jp/",
         icon: "mdi-office-building",
         color: "blue",
-        content:
-          "現在アルバイトをしているスタートアップです。\
-proofrog という、画像同士の比較ができる、印刷業界向けの校正検版ツールを開発しています。\
+        content: `現在アルバイトをしているスタートアップです。\
+<a href="https://proofrog.cloud/" target="_blank" rel="noopener noreferrer">proofrog</a>という、画像同士の比較ができる、\
+印刷業界向けの校正検版ツールを開発しています。\
 自分が担当しているのは、主にバックエンドですが、フロントエンドも必要に応じて担当することがあるので、\
 未熟ながらもフルスタックエンジニアのような働きができているのではないかと思います。\
-普段は、製品のコアである画像処理関連のアルゴリズムを実装しています。",
+普段は、製品のコアである画像処理関連のアルゴリズムを実装しています。`,
       },
       {
         yearMonth: "2019/12（2日間）",
@@ -199,7 +242,6 @@ proofrog という、画像同士の比較ができる、印刷業界向けの�
       {
         yearMonth: "2020/2",
         title: "株式会社RevComm入社（業務委託）",
-        url: "https://www.revcomm.co.jp/",
         icon: "mdi-office-building",
         color: "light-blue",
         content:
@@ -218,13 +260,12 @@ proofrog という、画像同士の比較ができる、印刷業界向けの�
       {
         yearMonth: "2020/4",
         title: "横浜市立大学大学院入学",
-        url: "https://www.yokohama-cu.ac.jp/ds/",
         icon: "mdi-school",
         color: "light-green",
         content:
-          "機械学習関連の勉強がしたかったので、データサイエンス研究科に入学。\
-          CS系大学院と迷ったが、卒業する年にちょうど新たな研究科が設立されたので運命を感じ、\
-          そのまま横浜市立大学の大学院へ進学。",
+          "機械学習関連の勉強がしたかったので、データサイエンス研究科に入学しました。\
+          CS系大学院と迷いましたが、卒業する年にちょうど新たな研究科が設立されたので、運命を感じ、\
+          そのまま横浜市立大学の大学院へ進学しました。",
       },
       {
         yearMonth: "2020/6",
@@ -236,8 +277,6 @@ proofrog という、画像同士の比較ができる、印刷業界向けの�
       {
         yearMonth: "2020/9（2週間）",
         title: "楽天株式会社インターン",
-        url:
-          "https://commerce-engineer.rakuten.careers/careers/graduates/internship/summer-short",
         icon: "mdi-office-building",
         color: "red",
         content:
@@ -261,5 +300,8 @@ a {
 }
 .expansion-panel {
   padding-top: 10px;
+  i {
+    margin-bottom: 3px;
+  }
 }
 </style>
